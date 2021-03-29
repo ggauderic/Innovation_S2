d43d3d4abbe1e25453bd1a511cce37eb03f9bb22
DRIIC <gauderic.gumbs@gmail.com>
2021-03-29 16:13:11 +0200

    Add marker on map by clicking

--- Mystic_Bikes_app/app/src/main/java/fr/esme/mystic_bikes_app/MainActivity.java
+++ Mystic_Bikes_app/app/src/main/java/fr/esme/mystic_bikes_app/MainActivity.java
@@ -1,9 +1,23 @@
-package fr.esme.mystic_bikes_app;import android.os.Bundle;
+package fr.esme.mystic_bikes_app;
+
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
 import com.mapbox.mapboxsdk.location.LocationComponentOptions;
@@ -13,11 +27,17 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.plugins.annotation.Symbol;
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager;
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions;
 
 import java.util.List;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import static com.mapbox.mapboxsdk.style.layers.Property.ICON_ANCHOR_BOTTOM;
 
 /**
  * Use the {@link LocationComponentOptions} builder's pulseEnabled()
@@ -25,10 +45,16 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class MainActivity extends AppCompatActivity implements
         OnMapReadyCallback, PermissionsListener {
-
+    public static final String ID_ICON = "id-icon";
     private PermissionsManager permissionsManager;
     private MapboxMap mapboxMap;
     private MapView mapView;
+    private Location originLocation;
+    private Button startButton;
+    private Point originPosition;
+    private Point destinationPosition;
+    SymbolManager symbolManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,17 +70,72 @@ public class MainActivity extends AppCompatActivity implements
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnLongClickListener(new View.OnLongClickListener(){
+
+            @Override
+            public boolean onLongClick(View v) {
+                return false;
+            }
+
+        });
     }
 
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
-        MainActivity.this.mapboxMap = mapboxMap;
-        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(getStyleBuilder(Style.MAPBOX_STREETS), new Style.OnStyleLoaded() {
             @Override
             public void onStyleLoaded(@NonNull Style style) {
                 enableLocationComponent(style);
+                symbolManager = new SymbolManager(mapView, mapboxMap, style);
+                symbolManager.setIconAllowOverlap(true);
+                symbolManager.setTextAllowOverlap(true);
             }
         });
+        this.mapboxMap.addOnMapClickListener(this::addSymbol);
+
+    }
+
+    private boolean addSymbol(LatLng point) {
+        if (symbolManager == null) {
+            return false;
+        }
+
+        symbolManager.create(new SymbolOptions()
+                .withLatLng(point)
+                .withIconImage(ID_ICON)
+                .withIconAnchor(ICON_ANCHOR_BOTTOM)
+                .withIconSize(1f)
+        );
+        //destinationPosition = Point.fromLngLat(point.getLongitude(), point.getLatitude());
+        //originPosition = Point.fromLngLat(originLocation.getLongitude(), originLocation.getLatitude());
+        startButton.setEnabled(true);
+
+        return true;
+    }
+
+    private Style.Builder getStyleBuilder(@NonNull String styleUrl) {
+        return new Style.Builder().fromUri(styleUrl)
+                .withImage(ID_ICON, generateBitmap(R.drawable.mapbox_ic_place));
+    }
+    private Bitmap generateBitmap(@DrawableRes int drawableRes) {
+        Drawable drawable = getResources().getDrawable(drawableRes);
+        return getBitmapFromDrawable(drawable);
+    }
+
+    static Bitmap getBitmapFromDrawable(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        } else {
+            // width and height are equal for all assets since they are ovals.
+            Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                    drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            return bitmap;
+        }
     }
 
     @SuppressWarnings( {"MissingPermission"})
@@ -71,6 +152,7 @@ public class MainActivity extends AppCompatActivity implements
 // Get an instance of the component
             LocationComponent locationComponent = mapboxMap.getLocationComponent();
 
+
 // Activate with options
             locationComponent.activateLocationComponent(
                     LocationComponentActivationOptions.builder(this, loadedMapStyle)
@@ -85,6 +167,7 @@ public class MainActivity extends AppCompatActivity implements
 
 // Set the component's render mode
             locationComponent.setRenderMode(RenderMode.NORMAL);
+            originLocation = locationComponent.getLastKnownLocation();
         } else {
             permissionsManager = new PermissionsManager(this);
             permissionsManager.requestLocationPermissions(this);
@@ -158,4 +241,6 @@ public class MainActivity extends AppCompatActivity implements
         super.onLowMemory();
         mapView.onLowMemory();
     }
+
+
 }
